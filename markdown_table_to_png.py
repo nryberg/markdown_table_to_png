9c9a53ca7efd1ed6a1ecfd4e147eda5181405482
nryberg <nryberg@gmail.com>
2023-11-04 10:09:19 -0500

    Moving CSS to external file

--- markdown_table_to_png.py
+++ markdown_table_to_png.py
@@ -1,37 +1,8 @@
 from html2image import Html2Image
 import markdown
 from PIL import Image, ImageOps
+import sys, getopt
 
-table_css = '''
-table {
-    border-collapse: collapse;
-    margin: 25px 0;
-    font-size: 0.9em;
-    font-family: sans-serif;
-    min-width: 400px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-}
-thead tr {
-    background-color: #3061b0;
-    color: #ffffff;
-    text-align: left;
-}
-th,
-td {
-    padding: 12px 15px;
-}
-tbody tr {
-    border-bottom: 1px solid #dddddd;
-}
-
-tbody tr:nth-of-type(even) {
-    background-color: #f3f3f3;
-}
-
-tbody tr:last-of-type {
-    border-bottom: 2px solid #3061b0;
-}
-'''
 def markdown_to_html(md):
     result = markdown.markdown(md,extensions=['tables'])
     return result
@@ -41,10 +12,7 @@ def html_to_png(html, file_name):
     hti.screenshot(html_str=html, css_str=table_css, save_as=file_name)
 
 def cropbox(file_name, output_file_name):
-
     img = Image.open(file_name)
-    # Get the content bounds.
-    content = img.getbbox(alpha_only=False)
 
     # Flip to RGB mode to enable invert operation.
     img_RGB=img.convert("RGB")
@@ -59,11 +27,15 @@ def cropbox(file_name, output_file_name):
     # Save the image.
     img.save(output_file_name)
 
+# Read in the CSS File
+with open('table.css', 'r') as f:
+    table_css = f.read()
+
+
 # Read markdown file
+
 with open('table.md', 'r') as f:
     md = f.read()
     html = markdown_to_html(md) # = markdown.markdown(md,extensions=['tables'])
     html_to_png(html, 'table.png')
     cropbox('table.png', 'fixed_table.png')
-#    print(md)
- #   'table.png'
